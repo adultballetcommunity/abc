60c4991429a6193787bcf29176ef14afd81e894d
adultballetcommunity <117054520+adultballetcommunity@users.noreply.github.com>
2022-10-31 15:13:04 +1100

    Add files via upload

---
+++ js/classSchedule.js
@@ -0,0 +1,158 @@
+const schedule = {
+  "Fenella": [
+    { // Mondays
+      day: 1,
+      studio: "Dance102",
+      classType: "B/E",
+      cost: 25,
+      time: {
+	start: 1000,
+	end: 1130
+      },
+      dates: [
+	"31.10.22",
+	"07.11.22",
+	"14.11.22"
+      ]
+    },
+    { // Thursdays
+      day: 4,
+      studio: "Dance102",
+      classType: "First Steps Centre & Pointe",
+      cost: 25,
+      time: {
+	start: 1900,
+	end: 2100
+      },
+      dates: [
+	"03.11.22",
+	"10.11.22",
+	"17.11.22"
+      ]
+    }
+  ],
+  "Ellena": [
+    { // Tuesdays
+      day: 2,
+      studio: "Dance102",
+      classType: "B/E",
+      cost: 25,
+      time: {
+	start: 1900,
+	end: 2030
+      },
+      dates: [
+	"01.11.22",
+	"20.12.22"
+      ]
+    },
+    { // Tuesdays
+      day: 2,
+      studio: "Dance102",
+      classType: "Beginner Pointe",
+      cost: 15,
+      time: {
+	start: 2030,
+	end: 2100
+      },
+      dates: [
+	"01.11.22",
+	"20.12.22"
+      ]
+    },
+  ],
+  "Mitch": [
+    { // Mondays
+      day: 1,
+      studio: "Dance102",
+      classType: "B/E",
+      cost: 20,
+      time: {
+	start: 1915,
+	end: 2045
+      },
+      dates: [
+	"07.11.22",
+	"14.11.22",
+	"21.11.22",
+	"28.11.22",
+	"05.12.22",
+	"12.12.22",
+	"19.12.22",
+	"26.12.22"
+      ]
+    },
+    { // Wednesdays
+      day:3,
+      studio: "Dance102",
+      classType: "B/E",
+      cost: 20,
+      time: {
+	start: 1830,
+	end: 2000
+      },
+      dates: [
+	"02.11.22",
+	"09.11.22",
+	"16.11.22",
+	"23.11.22",
+	"30.11.22",
+	"07.12.22",
+	"14.12.22",
+	"21.12.22",
+	"28.12.22"
+      ]
+    },
+  ],
+  "Fabio": [
+    { // Saturdays
+      day: 6,
+      studio: "Evolution",
+      classType: "Beg/Int",
+      cost: 22,
+      time: {
+	start: 1130,
+	end: 1300
+      },
+      dates: [
+	"05.11.22"
+      ]
+    },
+  ]
+};
+
+const studios = {
+  "Dance102": {
+    address: "575 Pacific Highway, St Leonards",
+    mapLink: "https://goo.gl/maps/M5HZtic5qryAez4DA",
+    phone: "(02) 9437 3900"
+  },
+  "Evolution": {
+    address: "1/15 Falcon St, Crows Nest",
+    mapLink: "https://goo.gl/maps/88ShHQQw6Bd3R2LM7",
+    phone: "0433 124 068"
+  }
+};
+
+const classTypes = {
+  "B": {
+    fullName: "Beginner",
+    description: "For dancers with solid base knowledge"
+  },
+  "B/E": {
+    fullName: "Beginner/Elementry",
+    description: "For medium-ability dancers"
+  }
+};
+
+const teachers = {
+  "Fenella": {
+    fullName: "",
+    image: "",
+    pricePerClass: 25,
+    paymentMethod: "cash"
+  },
+  "Ellena": {},
+  "Fabio": {},
+  "Mitch": {}
+};
---
+++ js/index.js
@@ -0,0 +1,174 @@
+const scheduleCont = document.getElementsByClassName("schedule")[0];
+const selectMenu = document.getElementById("scheduleSelect");
+
+selectMenu.value = localStorage.getItem("selectMenuValue") || "teacher";
+
+const renderSchedule = () => {
+  switch (selectMenu.value) {
+    case "teacher":
+      createScheduleTeacher();
+      break;
+    case "day":
+      createScheduleDay();
+      break;
+  }
+  localStorage.setItem("selectMenuValue", selectMenu.value);
+};
+
+selectMenu.addEventListener("input", renderSchedule);
+
+const c = (tag, attrs = {}, children = []) => {
+  const elem = document.createElement(tag);
+  for (const [attr, value] of Object.entries(attrs)) {
+    if (["textContent", "innerHTML", "outerHTML"].includes(attr)) {
+      elem[attr] = value;
+    } else {
+      elem.setAttribute(attr, value);
+    }
+  };
+  children.forEach(child => {
+    elem.appendChild(child);
+  });
+  return elem;
+};
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",  
+];
+
+// Equates arrays of strings
+const arrayOfStrEq = (arr1, arr2) => {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    console.error("Non-array passed to array equator", arr1, arr2);
+    return false;
+  }
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i=0; i<arr1.length; i++) {
+    if (typeof arr1[i] !== "string") {
+      console.error("Non-string-array", arr1);
+      return false;
+    }
+    if (typeof arr2[i] !== "string") {
+      console.error("Non-string-array", arr2);
+      return false;
+    }
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Converts 24hr time to am/pm time
+const timeConvert = (time) => {
+  if (!Number.isInteger(+time) || time < 0 || time > 2359) {
+    console.error("A non-24hr time was attemped conversion:", time);
+    return;
+  }
+  time = (typeof time === "string") ? time : time.toString();
+  const [hr, min, m] = [time.slice(0,2) % 12, time.slice(2), (time.slice(0,2) < 12) ? "am" : "pm"];
+  const newTime = `${hr}:${min}${m}`;
+  return newTime;
+};
+
+const writeTimeRange = (timeRange) => {
+  return `${timeConvert(timeRange.start)} - ${timeConvert(timeRange.end)}`;
+};
+
+const createScheduleDay = () => {
+  const table = c("table", {class: "schedule"});
+
+  // Create Schedule by Days
+  const scheduleDay = {};
+  days.forEach(day => {
+    scheduleDay[day] = [];
+  });
+  for (const [teacher, classes] of Object.entries(schedule)) {
+    classes.forEach(classe => {
+      classe.teacher = teacher;
+      scheduleDay[days[classe.day]].push(classe);
+    });
+  }
+
+  const daysOrder = days.slice(1);
+  daysOrder.push("Sunday");
+  daysOrder.forEach(day => {
+    const titleRow = c("tr", {class: "newSet"}, [
+      c("th", {class: "teacher", colspan: 4, textContent: day + "s"})
+    ]);
+    table.appendChild(titleRow);
+
+    scheduleDay[day].forEach((classe, i) => {
+      if (scheduleDay[day][i-1]?.teacher !== classe.teacher || !arrayOfStrEq(scheduleDay[day][i-1]?.dates, classe.dates)) {
+        let datesStr = classe.teacher + "&emsp;<span class='noBold'>(";
+        datesStr += (classe.dates.length === 1) ? `from ${classe.dates[0].split(/\./g).join("/")}` : `${classe.dates[0].split(/\./g).join("/")} to ${classe.dates[classe.dates.length-1].split(/\./g).join("/")}`;
+        datesStr += ")</span>";
+
+        const subTitleRow = c("tr", {}, [
+	  c("th", {class: "day", colspan: 4, innerHTML: datesStr})
+        ]);
+        table.appendChild(subTitleRow);
+      }
+      const infoRow = c("tr", {class: i===scheduleDay[day].length-1 ? "newSet" : ""}, [
+	c("td", {class: "time", textContent: writeTimeRange(classe.time)}),
+	c("td", {class: "studio", innerHTML: `<a href="${studios[classe.studio].mapLink}" target="_blank">${classe.studio}</a>`, title: studios[classe.studio].address}),
+	c("td", {class: "level", textContent: classe.classType}),
+	c("td", {class: "cost", textContent: "$" + classe.cost}),
+      ]);
+      table.appendChild(infoRow);
+    });
+  });
+
+  if (scheduleCont.getElementsByTagName("table")[0]) {
+    scheduleCont.getElementsByTagName("table")[0].replaceWith(table);
+  } else {
+    scheduleCont.appendChild(table);
+  }
+};
+
+const createScheduleTeacher = () => {
+  const table = c("table", {class: "schedule"});
+
+  for (const [teacher, classes] of Object.entries(schedule)) {
+    const titleRow = c("tr", {class: "newSet"}, [
+      c("th", {class: "teacher", colspan: 4, textContent: teacher})
+    ]);
+    table.appendChild(titleRow);
+
+    classes.forEach((classe, i) => {
+      if (classes[i-1]?.day !== classe.day || !arrayOfStrEq(classes[i-1]?.dates, classe.dates)) {
+        let datesStr = days[classe.day] + "s" + "&emsp;<span class='noBold'>(";
+        datesStr += (classe.dates.length === 1) ? `from ${classe.dates[0].split(/\./g).join("/")}` : `${classe.dates[0].split(/\./g).join("/")} to ${classe.dates[classe.dates.length-1].split(/\./g).join("/")}`;
+        datesStr += ")</span>";
+
+        const subTitleRow = c("tr", {}, [
+	  c("th", {class: "day", colspan: 4, innerHTML: datesStr})
+        ]);
+        table.appendChild(subTitleRow);
+      }
+      const infoRow = c("tr", {class: i===classes.length-1 ? "newSet" : ""}, [
+	c("td", {class: "time", textContent: writeTimeRange(classe.time)}),
+	c("td", {class: "studio", innerHTML: `<a href="${studios[classe.studio].mapLink}" target="_blank">${classe.studio}</a>`, title: studios[classe.studio].address}),
+	c("td", {class: "level", textContent: classe.classType}),
+	c("td", {class: "cost", textContent: "$" + classe.cost}),
+      ]);
+      table.appendChild(infoRow);
+    });
+  }
+
+  if (scheduleCont.getElementsByTagName("table")[0]) {
+    scheduleCont.getElementsByTagName("table")[0].replaceWith(table);
+  } else {
+    scheduleCont.appendChild(table);
+  }  
+};
+
+renderSchedule();
